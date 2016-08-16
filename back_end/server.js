00b0b28f18840d98ba12f3b53624852442ce8ab9
awdesh <awdesh.sharma1@gmail.com>
2016-08-15 21:45:12 -0700

    updates on POST

--- back_end/server.js
+++ back_end/server.js
@@ -3,46 +3,43 @@
 
 // init project
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
 var _ = require('underscore');
 var path = require('path');
-// var db = require('db.js');
+var db = require('./db.js');
+var PORT = process.env.PORT || 3000;
+var IP = process.env.IP;
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+app.use(bodyParser.json());
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static(__dirname + '/../front_end'));
 	console.log(path.resolve(__dirname ,'../front_end/views/index.html'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  console.log(path.resolve(__dirname ,'../front_end/views/index.html'));
-	response.sendFile(path.resolve(__dirname ,'../front_end/views/index.html'));
+app.get("/", function (req, res) {
+	res.sendFile(path.resolve(__dirname ,'../front_end/views/index.html'));
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.get("/activities", function (req, res) {
+  console.log('activities get called');
+  res.send(activities);
 });
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/activities", function (request, response) {
-  console.log(request.query.activity);
-  activities.push(request.query.activity);
-  response.sendStatus(200);
+app.post("/activities", function (req, res) {
+  var body = _.pick(req.body, 'personalText', 'professionalText', 'otherText');
+  console.log("body is-:" + body);
+  db.activities.create(body).then(function(activities){
+    if(activities)
+        return res.json(activities.toJSON());
+    else
+        return res.status(404).send();
+    }, function(e){
+        return res.status(404).json(e);
+  });
 });
-
-// app.post('/todos', middleware.requireAuthentication, function(req, res){
-//     var body = _.pick(req.body, 'description', 'completed');
-//     db.todo.create(body).then(function(todo){
-//         if(todo)
-//             return res.json(todo.toJSON());
-//         else
-//             return res.status(404).send();
-//     }, function(e){
-//         return res.status(404).json(e);
-//     });
-// });
 
 // Simple in-memory store for now
 var activities = [
@@ -51,7 +48,9 @@ var activities = [
   "Wash the dishes"
   ];
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+db.sequelize.sync({force: true}).then(function(){
+  app.listen(PORT, function() {
+      console.log('Express listening on port ' + PORT);
+    });
 });
+
--- front_end/public/client.js
+++ front_end/public/client.js
@@ -1,43 +1,49 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+//     $.ajax({
+//       type: "GET",
+//       dataType: "json",
+//         url: "/",
+//         success: function(data){
+//       $('.greeting-id').append(data.id);          
+//       $('.greeting-content').append(data.content);          
+//       },
+//       error: function(status){
+//         console.log('error occurred calling endpoint' + status);
+//       }
+//   });
+// })
+
+$(function() {
+  
+  $.get('/activities', function(activities) {
+    activities.forEach(function(activity) {
+    console.log('activities called ' + activity);
+
+      $('<li></li>').text(activity).appendTo('ul#activities');
+    });
+  });
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+    var personalText = $('.personalText').val();
+    var professionalText = $('.professionalText').val();
+    var otherText = $('.otherText').val();
+    console.log(personalText + professionalText + otherText);
+    var formInput = { "personalText" : personalText, "professionalText" : professionalText, "otherText" : otherText };
     $.ajax({
-      type: "GET",
+      type: "POST",
       dataType: "json",
-        url: "/activities",
-        success: function(data){
-      $('.greeting-id').append(data.id);          
-      $('.greeting-content').append(data.content);          
+      url: "/activities",
+      contentType: 'application/json',
+      data: JSON.stringify(formInput),
+      success: function(r){
+        console.log('Thanks for sending the data for today!');
       },
       error: function(status){
         console.log('error occurred calling endpoint' + status);
       }
-  });
-})
-
-// client-side js
-// run by the browser each time your view template is loaded
-
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
-
-$(function() {
-  
-  // $.get('/', function(dreams) {
-  //   dreams.forEach(function(dream) {
-  //     $('<li></li>').text(dream).appendTo('ul#dreams');
-  //   });
-  // });
-
-  $('form').submit(function(event) {
-    event.preventDefault();
-    activity = $('.otherText').val();
-    console.log(activity);
-    $.post('/activities?' + $.param({activity: activity}), function() {
-      $('<li></li>').text(activity).appendTo('ul#activities');
-      $('input').val('');
-      $('input').focus();
     });
   });
-
 });
 
 
