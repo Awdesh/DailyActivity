5c822986ffcfd27a07cab142ced1bdf985d8f971
awdesh <awdesh.sharma1@gmail.com>
2016-08-21 21:13:43 -0700

    get http action uppdated.

--- front_end/public/client.js
+++ front_end/public/client.js
@@ -1,52 +1,57 @@
-if (typeof DailyActivityAPI === "undefined") {
-  (function() {
+$(function() {
+  $(".myButton").click(function() {
 
-    var DailyActivityAPI = window.DailyActivityAPI =
-      (function() {
-        $('form').submit(function(event) {
-          event.preventDefault();
-          var personalText = $('.personalText').val();
-          var professionalText = $('.professionalText').val();
-          var otherText = $('.otherText').val();
-          console.log(personalText + professionalText + otherText);
-          var formInput = {
-            "personalText": personalText,
-            "professionalText": professionalText,
-            "otherText": otherText
-          };
-          $.ajax({
-            type: "POST",
-            dataType: "json",
-            url: "/activities",
-            contentType: 'application/json',
-            data: JSON.stringify(formInput),
-            success: function(r) {
-              console.log('Good job recording today\'s activity!');
-            },
-            error: function(status) {
-              console.log('error occurred calling endpoint' + status);
-            }
-          });
-        });
-
-        var getActivity = function() {
-          $.ajax({
-            type: "GET",
-            dataType: "json",
-            url: "/activities",
-            success: function(data) {
-              console.log('called success!!');
-              $('.activities').append(data.id);
-            },
-            error: function(status) {
-              console.log('error occurred calling endpoint' + status);
-            }
-          });
-
-        };
+    $.get('/activities', function(activities) {
+      activities.forEach(function(activity) {
+        console.log(activity);
+        console.log('called get');
       });
-  })();
-}
+    });
+  });
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+    var personalText = $('.personalText').val();
+    var professionalText = $('.professionalText').val();
+    var otherText = $('.otherText').val();
+    console.log(personalText + professionalText + otherText);
+    var formInput = {
+      "personalText": personalText,
+      "professionalText": professionalText,
+      "otherText": otherText
+    };
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: "/activities",
+      contentType: 'application/json',
+      data: JSON.stringify(formInput),
+      success: function(r) {
+        console.log('Good job recording today\'s activity!');
+      },
+      error: function(status) {
+        console.log('error occurred calling endpoint' + status);
+      }
+    });
+  });
+});
+
+// var getActivity = function() {
+//   $.ajax({
+//     type: "GET",
+//     dataType: "json",
+//     url: "/activities",
+//     success: function(data) {
+//       console.log('called success!!');
+//       $('.activities').append(data.id);
+//     },
+//     error: function(status) {
+//       console.log('error occurred calling endpoint' + status);
+//     }
+//   });
+
+// };
+
 // $.get('/activities', function(activities) {
 //   activities.forEach(function(activity) {
 //   console.log('activities called ' + activity);
@@ -85,4 +90,4 @@ if (typeof DailyActivityAPI === "undefined") {
 //         console.log('error occurred calling endpoint' + status);
 //       }
 //   });
-// });
+// });      // }););      // });
